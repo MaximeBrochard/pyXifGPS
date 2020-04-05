7a52a30bf817331ea515ff31e3109713d62d5e13
maxime <maxime.brochard94@gmail.com>
2020-04-05 19:54:16 +0200

    check if exif contains GPSinfo

--- src/exif_gps.py
+++ src/exif_gps.py
@@ -12,6 +12,7 @@ import piexif
 from GPSPhoto import gpsphoto
 import exifread
 
+
 def get_files(path, extensions):
     all_files = []
     for ext in extensions:
@@ -20,7 +21,6 @@ def get_files(path, extensions):
 
 
 def read_gpx_dataframe(file_name, deltaMS):
-
     with open(file_name, "r") as gpx_file:
         gpx = gpxpy.parse(gpx_file)
 
@@ -53,14 +53,14 @@ def read_gpx_dataframe(file_name, deltaMS):
     return df
 
 
-#Get the exif of a file
+# Get the exif of a file
 def get_exif(filename):
     image = Image.open(filename)
     image.verify()
     return image._getexif()
 
 
-#Get the labeled exif of a raw _getexif() def
+# Get the labeled exif of a raw _getexif() def
 def get_labeled_exif(exif):
     labeled = {}
     for (key, val) in exif.items():
@@ -81,11 +81,24 @@ def get_Tag(exif, targetTag):
     return ret[targetTag]
 
 
+def is_Tag(exif, param):
+    ret = {}
+    for (tag, value) in exif.items():
+        decoded = TAGS.get(tag)
+        ret[decoded] = value
+
+    if param in ret:
+        return True
+    else:
+        return False
+
+
 def increment_time(timeString, seconds):
     a = datetime.datetime.strptime(timeString, "%Y:%m:%d %H:%M:%S")
     b = a + datetime.timedelta(seconds=seconds)
     c = b.strftime("%Y:%m:%d %H:%M:%S")
     return c
+
 
 def to_deg(value, loc):
     """convert decimal coordinates into degrees, munutes and seconds tuple
@@ -99,10 +112,10 @@ def to_deg(value, loc):
     else:
         loc_value = ""
     abs_value = abs(value)
-    deg =  int(abs_value)
-    t1 = (abs_value-deg)*60
+    deg = int(abs_value)
+    t1 = (abs_value - deg) * 60
     min = int(t1)
-    sec = round((t1 - min)* 60, 5)
+    sec = round((t1 - min) * 60, 5)
     return (deg, min, sec, loc_value)
 
 
@@ -115,7 +128,7 @@ def change_to_rational(number):
     return (f.numerator, f.denominator)
 
 
-def add_gps_infos(file_name, lat, lng, altitude):
+def add_gps_infos(file_name, lat, lng, altitude, date):
     """Adds GPS position as EXIF metadata
     Keyword arguments:
     file_name -- image file
@@ -146,10 +159,14 @@ def add_gps_infos(file_name, lat, lng, altitude):
 
     # update original exif data to include GPS tag
     exif_data.update(gps_exif)
-    exif_data['Exif'][41729] = b'1'
-    exif_bytes = piexif.dump(exif_data)
 
+    # correct bug with poor-exif devices (smartphones...)
+    exif_data['Exif'][41729] = b'1'
+
+    # push updated exif in file
+    exif_bytes = piexif.dump(exif_data)
     piexif.insert(exif_bytes, file_name)
+
 
 def analyse_single_photo(path_single_photo, path_gpx_file, utc_offset_seconds):
     from pathlib import Path
@@ -163,18 +180,21 @@ def analyse(img, parsed_gpx):
 
     exif = get_exif(img)
     dateOriginal = get_Tag(exif, 'DateTimeOriginal')
-    #print(f"Analysing '{img.name}', taken at {dateOriginal}")
+
+    if is_Tag(exif, 'GPSInfo'):
+        print(f"[{img.name}]    GPSInfo already in exif")
 
     if dateOriginal < parsed_gpx.iloc[0]['dateTime']:
         print(
             f"[{img.name}]  Picture taken before gpx track starts. lat: {parsed_gpx.iloc[0]['latitude']}, long: {parsed_gpx.iloc[0]['longitude']}, altitude: {parsed_gpx.iloc[-1]['elevation']}")
-        add_gps_infos(my_path, parsed_gpx.iloc[0]['latitude'], parsed_gpx.iloc[0]['longitude'], int(parsed_gpx.iloc[0]['elevation']))
+        add_gps_infos(my_path, parsed_gpx.iloc[0]['latitude'], parsed_gpx.iloc[0]['longitude'],
+                      int(parsed_gpx.iloc[0]['elevation']))
 
     if dateOriginal > parsed_gpx.iloc[-1]['dateTime']:
         print(
             f"[{img.name}]  Picture taken after gpx track ended. lat: {parsed_gpx.iloc[-1]['latitude']}, long: {parsed_gpx.iloc[-1]['longitude']}, altitude: {parsed_gpx.iloc[-1]['elevation']}")
-        add_gps_infos(my_path, parsed_gpx.iloc[-1]['latitude'], parsed_gpx.iloc[-1]['longitude'], int(parsed_gpx.iloc[-1]['elevation']))
-
+        add_gps_infos(my_path, parsed_gpx.iloc[-1]['latitude'], parsed_gpx.iloc[-1]['longitude'],
+                      int(parsed_gpx.iloc[-1]['elevation']))
 
     previous_point = None
     for index, point in parsed_gpx.iterrows():
@@ -192,14 +212,13 @@ def analyse(img, parsed_gpx):
                     if time_cursor == dateOriginal:
                         print(
                             f"[{img.name}]  Matching trackpoint at {point['dateTime']} : lat:{point['latitude']} long: {point['longitude']} altitude:{point['elevation']}m")
-                        add_gps_infos(my_path, point['latitude'], point['longitude'], int(point['elevation']))
+                        add_gps_infos(my_path, point['latitude'], point['longitude'], int(point['elevation']),
+                                      point['dateTime'])
         previous_point = point
 
 
 def analyse_in_dir(path_photos_dir, path_gpx_file, utc_offset_seconds):
-
     parsed_gpx = read_gpx_dataframe(path_gpx_file, utc_offset_seconds)
-
 
     from pathlib import Path
     p = Path(path_photos_dir)
@@ -208,8 +227,8 @@ def analyse_in_dir(path_photos_dir, path_gpx_file, utc_offset_seconds):
         analyse(img, parsed_gpx)
 
 
-#analyse_single_photo("../data/sample-6/sampledata-6.jpg", "../data/sample-6/sampledata-6.gpx", 7200)
-#analyse_in_dir("../data/sample-2-lite", "../data/sample-2-lite/sampledata-2.gpx", 7200)
+# analyse_single_photo("../data/sample-7/sampledata-7.jpg", "../data/sample-7/sampledata-7.gpx", 7200)
+# analyse_in_dir("../data/sample-2-lite", "../data/sample-2-lite/sampledata-2.gpx", 7200)
 
 
 if __name__ == "__main__":
@@ -228,12 +247,11 @@ if __name__ == "__main__":
        **Like previous example** with adjusting gpx timezone at GMT+2 (7200s)
            python exif_gps.py ./example.gpx  --singlefile ./image.jpg --offset 7200""")
 
-
     )
     parser.add_argument(
         "--directory",
         help="Analyse all image files in the directory. When this flag is true, "
-        "the last argument must be a directory that contains several *.jpg",
+             "the last argument must be a directory that contains several *.jpg",
     )
 
     parser.add_argument(
@@ -249,7 +267,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--offset",
         help="Offset (in seconds) between images and *.gpx track"
-        """Example:
+             """Example:
          When using 'example.gpx' containing <time> values expressed as GMT Timestamp (i.e ending with a Z),
           and 'example.jpg', an image taken during 'example.jpg', the picture may have a different capture TimeZone.
           Saying the picture is taken in Paris (GMT+2), offset argument's should be '-offset 7200', because 'GMT+2' = 2hours = 7200seconds
@@ -268,4 +286,3 @@ if __name__ == "__main__":
     else:
         print(f"Analysing single file '{args.singlefile}'")
         analyse_single_photo(args.singlefile, args.trace, args.offset)
-
